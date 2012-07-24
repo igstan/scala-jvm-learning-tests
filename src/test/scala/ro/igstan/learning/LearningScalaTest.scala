45dc2a4d3634f842470a2ada1ce3ddd5d6cfe911
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-07-24 15:09:29 +0300

    Rename test class

--- src/test/scala/ro/igstan/learning/LearningScalaTest.scala
+++ src/test/scala/ro/igstan/learning/LearningScalaTest.scala
@@ -3,7 +3,7 @@ package ro.igstan.learning
 import org.scalatest.FunSpec
 import org.scalatest.matchers.MustMatchers
 
-class Learning extends FunSpec with MustMatchers {
+class LearningScalaTest extends FunSpec with MustMatchers {
   describe("what") {
     it("does...") {
       true must be(true)
