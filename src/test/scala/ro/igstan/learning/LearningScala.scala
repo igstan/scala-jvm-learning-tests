0ab9c275084c215606fc8e0bfd3b5a01ee64b567
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-08-01 13:41:36 +0300

    Learn that overriding a var needs type annotation

--- src/test/scala/ro/igstan/learning/LearningScala.scala
+++ src/test/scala/ro/igstan/learning/LearningScala.scala
@@ -21,4 +21,19 @@ class LearningScala extends FunSpec with MustMatchers {
       trait B extends A
     }
   }
+
+  describe("a class with an abstract var") {
+    it("requires type annotation when defining the var in a subclass") {
+      trait T
+
+      abstract class A {
+        var t: T
+      }
+
+      class B extends A {
+        // var t = new T { } // does not compile
+        var t: T = new T { } // compiles
+      }
+    }
+  }
 }
