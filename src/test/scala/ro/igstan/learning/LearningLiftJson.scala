bb2bfd670db971654c186f1bc03ca06131333f8f
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-08-15 15:38:41 +0300

    Learn how to parse JSON structures into predefined case classes

--- src/test/scala/ro/igstan/learning/LearningLiftJson.scala
+++ src/test/scala/ro/igstan/learning/LearningLiftJson.scala
@@ -3,14 +3,24 @@ package ro.igstan.learning
 import org.scalatest.FunSpec
 import org.scalatest.matchers.MustMatchers
 
-import net.liftweb.json.parse
+import net.liftweb.json.DefaultFormats
 import net.liftweb.json.JsonAST._
+import net.liftweb.json.parse
+
+case class Sample(key: String)
 
 class LearningLiftJson extends FunSpec with MustMatchers {
+  implicit val formats = DefaultFormats
+
   describe("using the `parse` method") {
     it("will produce the expected JsonAST structure") {
       val ast = parse("""{ "key": "value" }""")
       ast must be(JObject(List(JField("key", JString("value")))))
+    }
+
+    it("parses the string into a case class") {
+      val sample = parse("""{ "key": "value" }""").extract[Sample]
+      sample must be (Sample("value"))
     }
   }
 }
