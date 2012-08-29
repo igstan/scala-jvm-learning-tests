10c62a98631c24a73206db45f32263af0756724d
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-08-29 14:27:14 +0300

    Learn how to deserialize polymorphic types using lift-json

--- src/test/scala/ro/igstan/learning/LearningLiftJson.scala
+++ src/test/scala/ro/igstan/learning/LearningLiftJson.scala
@@ -3,12 +3,17 @@ package ro.igstan.learning
 import org.scalatest.FunSpec
 import org.scalatest.matchers.MustMatchers
 
-import net.liftweb.json.DefaultFormats
+import net.liftweb.json.{ DefaultFormats, Formats, ShortTypeHints, TypeHints }
 import net.liftweb.json.JsonAST._
 import net.liftweb.json.Serialization.{ read, write }
 import net.liftweb.json.parse
 
 case class Sample(key: String)
+
+trait Common
+case class Foo(foo: String) extends Common
+case class Bar(bar: String) extends Common
+case class Baz(baz: String) extends Common
 
 class LearningLiftJson extends FunSpec with MustMatchers {
   implicit val formats = DefaultFormats
@@ -27,6 +32,43 @@ class LearningLiftJson extends FunSpec with MustMatchers {
     it("serializes a case class into a correct string") {
       val json = write(Sample("value"))
       json must be ("""{"key":"value"}""")
+    }
+
+    it("parses a polymorphic type using ShortTypeHints") {
+      implicit val formats = new Formats {
+        val dateFormat = DefaultFormats.lossless.dateFormat
+        override val typeHintFieldName = "type"
+        override val typeHints = ShortTypeHints(List(classOf[Foo], classOf[Bar], classOf[Baz]))
+      }
+
+      val foo = read[Common]("""{
+        "type": "Foo",
+        "foo": "oof"
+      }""")
+
+      foo must be (Foo(foo="oof"))
+    }
+
+    // Adapted from this blog post:
+    // http://www.scalafied.com/105/default-and-customized-lift-json-type-hints
+    it("parses a polymorphic type using custom TypeHints") {
+      case class LowerCaseTypeHints(hints: List[Class[_]]) extends TypeHints {
+        def hintFor(klass: Class[_]): String = klass.getSimpleName.toLowerCase
+        def classFor(hint: String) = hints find (hintFor(_) == hint)
+      }
+
+      implicit val formats = new Formats {
+        val dateFormat = DefaultFormats.lossless.dateFormat
+        override val typeHintFieldName = "type"
+        override val typeHints = LowerCaseTypeHints(List(classOf[Foo], classOf[Bar], classOf[Baz]))
+      }
+
+      val foo = read[Common]("""{
+        "type": "foo",
+        "foo": "oof"
+      }""")
+
+      foo must be (Foo(foo="oof"))
     }
   }
 }
