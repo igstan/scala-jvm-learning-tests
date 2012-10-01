5dacd95f335bc4e8182d55465ad4ef67aae2598a
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-10-01 23:17:09 +0300

    Learn the difference between `equals` and `isEqual` in JodaTime

--- src/test/scala/ro/igstan/learning/LearningJodaTime.scala
+++ src/test/scala/ro/igstan/learning/LearningJodaTime.scala
@@ -1,5 +1,6 @@
 package ro.igstan.learning
 
+import org.joda.time.{ DateTime, DateTimeZone }
 import org.joda.time.format.ISODateTimeFormat
 import org.scalatest.FunSpec
 import org.scalatest.matchers.MustMatchers
@@ -30,13 +31,32 @@ class LearningJodaTime extends FunSpec with MustMatchers {
     }
   }
 
-  describe("equality") {
+  describe("equals") {
     it ("reports two identical dates as equal") {
-      val formatter = ISODateTimeFormat.date();
-      val date1 = formatter.parseDateTime("2012-01-03");
-      val date2 = formatter.parseDateTime("2012-01-03");
-
+      val formatter = ISODateTimeFormat.date
+      val date1 = formatter.parseDateTime("2012-01-03")
+      val date2 = formatter.parseDateTime("2012-01-03")
       assert(date1 === date2)
+    }
+
+    it ("two DateTime instances are not equal if they have different Chronologies") {
+      val formatter = ISODateTimeFormat.dateTimeNoMillis
+      val date1 = formatter.withZone(DateTimeZone.forID("Europe/Bucharest"))
+                           .parseDateTime("2012-01-03T00:00:00Z")
+      val date2 = new DateTime(2012, 1, 3, 0, 0, DateTimeZone.UTC)
+
+      date1 must not(be(date2))
+    }
+  }
+
+  describe("isEqual") {
+    it ("ignores chronologies and timezones") {
+      val formatter = ISODateTimeFormat.dateTimeNoMillis
+      val date1 = formatter.withZone(DateTimeZone.forID("Europe/Bucharest"))
+                           .parseDateTime("2012-01-03T00:00:00Z")
+      val date2 = new DateTime(2012, 1, 3, 0, 0, DateTimeZone.UTC)
+
+      assert(date1 isEqual date2)
     }
   }
 }
